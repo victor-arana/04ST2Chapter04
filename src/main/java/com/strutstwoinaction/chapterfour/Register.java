cd89949f918a00c3b180cc3293794d4c68571517
Victor Jose Arana Rodriguez <victor_arana@hotmail.com>
2015-08-09 10:40:13 -0500

    ModelDriven Action

--- src/main/java/com/strutstwoinaction/chapterfour/Register.java
+++ src/main/java/com/strutstwoinaction/chapterfour/Register.java
@@ -12,43 +12,22 @@ import com.strutstwoinaction.chapterfour.utils.User;
  * validation and localize our message texts with out polluting the
  * execute() method of our action.   
  */
-public class Register extends ActionSupport{
+public class Register extends ActionSupport implements ModelDriven{
 
 	private static final long serialVersionUID = 3327714661652096128L;
 
 	@Override
 	public String execute() throws Exception {
-		User user = new User();
-		user.setPassword(getPassword());
-		user.setPortfolioName(getPortfolioName());
-		user.setUsername(getUsername());
-		
 		getPortfolioService().createAccount(user);
 		return SUCCESS;
 	}
 	
-	/* JavaBeans Properties to Receive Request Parameters */	
-	private String username;
-	private String password;
-	private String portfolioName;
-
-	public String getPortfolioName() {
-		return portfolioName;
-	}
-	public void setPortfolioName(String portfolioName) {
-		this.portfolioName = portfolioName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
+	
+	/* JavaBeans Properties to Receive Request Parameters */
+	private User user = new User();
+	@Override
+	public Object getModel() {
+		return user;
 	}
 
 	/* Validateable Implementation 
@@ -73,18 +52,18 @@ public class Register extends ActionSupport{
 		PortfolioService ps = getPortfolioService();
 		
 		/* Check that fields are not empty */
-		if(getPassword().length() == 0){
+		if(user.getPassword().length() == 0){
 			addFieldError("password", getText("password.required"));
 		}
-		if(getUsername().length() == 0){
+		if(user.getUsername().length() == 0){
 			addFieldError("username", getText("username.required"));
 		}
-		if(getPortfolioName().length() == 0){
+		if(user.getPortfolioName().length() == 0){
 			addFieldError("portfolioName", getText("portfolioName.required"));
 		}
 		/* Make sure user doesn't already have an account */
-		if(ps.userExists(getUsername())){
-			addFieldError("user.username", getText("user.exists"));
+		if(ps.userExists(user.getUsername())){
+			addFieldError("username", getText("user.exists"));
 		}
 	}
 	
@@ -98,4 +77,5 @@ public class Register extends ActionSupport{
 		return new PortfolioService();
 		
 	}
+
 }
